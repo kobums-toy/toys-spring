8a654d3aa806cd70d5450202406ca037a448736e
고범석 <kobums@naver.com>
2025-02-06 15:30:22 +0900

    make fiel

---
+++ src/main/java/com/example/toysspring/ToysspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.toysspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toysspring/ToysspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toysspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
